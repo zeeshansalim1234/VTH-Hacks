cb159b743bbf01d82403a338c654211e879d1097
sharansarthak <59523742+sharansarthak@users.noreply.github.com>
2022-02-26 05:06:22 -0700

    Printing name, email and password from flask

--- everything/api.py
+++ everything/api.py
@@ -1,7 +1,7 @@
-from django.shortcuts import redirect
-from flask import Flask, request, jsonify, make_response, send_from_directory, send_file, render_template, url_for
+from flask import Flask, request, json, make_response, send_from_directory, send_file, render_template, url_for
 import os
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 CORS(app)
@@ -13,21 +13,25 @@ def home():
 
 @app.route('/login')
 def login():
-    print("Hi")
     return render_template('login.html')
 
 @app.route('/showSignUp')
-def showSignUP():
+def showSignUp():
     return render_template('login.html')
 
 
 @app.route('/signup', methods = ['POST', 'GET'])
-def signUp(): 
-    print("YES")
-    _name = request.form.get('Username')
-    _email = request.form.get('Email')
-    _password = request.form.get('Password')
-    print(_name)
+def signup():
+
+    data = request.data
+    _name = request.form.get("inputName")
+    _email = request.form.get("email")
+    _password = request.form.get("password")
+    if _name and _email and _password:
+        print(_name)
+        print(_email)
+        print(_password)
+
     return render_template('login.html')
 
 
